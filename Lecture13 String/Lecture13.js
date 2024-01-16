68490888782f86e95785c6964706b1117a33adc9
Pankaj Kumar Bind <pankajbind30@gmail.com>
2024-01-16 22:11:57 +0530

    Lecture 13 added

---
+++ Lecture13 String/Lecture13.js
@@ -0,0 +1,20 @@
+// string
+let name = "pankaj"
+console.log(name.length)
+console.log(name[0])
+console.log(name[1])
+
+for(let i=0; i<=name.length; i++){
+    console.log(name[i])
+}
+
+// Templet literals
+let boy1 = "Pankaj"
+let boy2 = "Neeraj"
+let sentence = `${boy1} is brother of ${boy2}`;
+console.log(sentence)
+
+// Escape sequence
+let fruit = "bana\'na"
+console.log(fruit)
+console.log(fruit.length)
