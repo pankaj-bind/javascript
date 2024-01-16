da139aab12437e24c2990a079c330541a8224474
Pankaj Kumar Bind <pankajbind30@gmail.com>
2024-01-16 19:51:09 +0530

    Lecture 11 added

---
+++ Lecture11 functions/Lecture11.js
@@ -0,0 +1,25 @@
+// function
+
+let a = prompt("Enter the value of a: ")
+let b = prompt("Enter the value of b: ")
+sum(a,b);
+function sum(a,b){
+    console.log(a+b);
+    let c = a+b;
+    console.log(typeof c) //return string
+}
+
+a=Number.parseInt(a);
+b=Number.parseInt(b);
+sum(a,b);
+function sum(a,b){
+    let c = a+b;
+    console.log(typeof c) //return number
+}
+
+// constant function
+
+const add=(p,q,r)=>{
+    return p+q+q;
+}
+console.log(add(3,5,5));
