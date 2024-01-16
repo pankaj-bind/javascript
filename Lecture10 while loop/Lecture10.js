5eb286a63ba2d760d190a33485d84f8e9b855dd9
Pankaj Kumar Bind <pankajbind30@gmail.com>
2024-01-16 19:32:49 +0530

    Lecture 10 added

---
+++ Lecture10 while loop/Lecture10.js
@@ -0,0 +1,15 @@
+//  while loop
+let a = prompt("Enter a value: ");
+a = Number.parseInt(a);
+// while(a<0){
+//     console.log(a);
+//     a--;
+// }
+
+// do while
+
+let b=0;
+do{
+console.log(b);
+b++;
+}while(b<a)
