db582a6f7ab59a8249a83dd84c44dd2491d67438
Pankaj Kumar Bind <pankajbind30@gmail.com>
2024-01-19 02:30:41 +0530

    Lecture 32 added

--- Lecture32 accessing children of an element/Lecture32.js
+++ Lecture32 accessing children of an element/Lecture32.js
@@ -1,5 +1,5 @@
 console.log(document.body.firstChild)
 console.log(document.body.lastChild)
-console.log(document.body.childNodes)
+console.log(document.body.childNodes[1])
 let arr = Array.from(document.body.childNodes)
 console.log(arr)
