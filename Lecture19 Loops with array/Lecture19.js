30f73124500fa2b95c12ad82151a537a50a3b700
Pankaj Kumar Bind <pankajbind30@gmail.com>
2024-01-18 09:57:16 +0530

    lecture 19 added

---
+++ Lecture19 Loops with array/Lecture19.js
@@ -0,0 +1,22 @@
+//  loops with array
+// simple for loop
+var arr = [1,2,3,4,5,6,7,8,9,10];
+for(let i=0; i<=arr.length; i++){
+    console.log(arr[i])
+}
+
+// forEach
+arr.forEach((element)=>{
+    console.log(element)
+}
+)
+
+// for in
+for(let i in arr){
+    console.log(arr[i])
+}
+
+// for of
+for(let i of arr){
+    console.log(arr[i])
+}
