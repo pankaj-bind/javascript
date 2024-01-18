0620ebb9fdef2812bf0cc1d20c59d3224045abcf
Pankaj Kumar Bind <pankajbind30@gmail.com>
2024-01-18 12:03:09 +0530

    lecture 23 added

---
+++ Lecture23 JS in browser/Lecture23.js
@@ -0,0 +1,30 @@
+/*
+JavaScript is a versatile and widely-used programming language that is primarily used for building interactive and dynamic web pages. When it comes to web development, JavaScript is mainly executed in web browsers, making it an essential part of front-end development. Here's a brief introduction:
+
+Purpose:
+
+JavaScript is designed to make web pages interactive and dynamic.
+It allows developers to manipulate and update the content of a webpage in real-time, responding to user actions.
+Execution Environment:
+
+JavaScript is primarily executed in web browsers, enabling client-side scripting.
+Modern browsers have built-in JavaScript engines (e.g., V8 in Chrome, SpiderMonkey in Firefox) that interpret and execute JavaScript code.
+Features:
+
+Event-Driven: JavaScript is event-driven, responding to user actions like clicks, inputs, and page loads.
+Asynchronous: Supports asynchronous programming, allowing non-blocking execution of code to enhance performance.
+Object-Oriented: Follows a prototype-based object-oriented programming paradigm.
+Use Cases:
+
+DOM Manipulation: JavaScript is commonly used to manipulate the Document Object Model (DOM), enabling changes to webpage content and structure dynamically.
+Form Validation: It's often employed to validate user input on forms before submitting data to a server.
+AJAX: Enables asynchronous communication with servers, updating parts of a webpage without requiring a full page reload.
+Animations: JavaScript is utilized for creating dynamic and interactive animations on web pages.
+Integration:
+
+JavaScript is typically embedded directly within HTML documents or included as external script files using the <script> tag.
+It can also be used in conjunction with HTML and CSS to create a complete web application.
+Frameworks and Libraries:
+
+There are numerous JavaScript libraries and frameworks, such as React, Angular, and Vue.js, which simplify and enhance the development of complex web applications.
+*/
