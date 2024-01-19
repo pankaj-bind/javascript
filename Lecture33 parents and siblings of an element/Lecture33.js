b51032cb83f3cf60c4c4fd5c80dfa596dd16f7b4
Pankaj Kumar Bind <pankajbind30@gmail.com>
2024-01-19 14:27:00 +0530

    Lecture 33 added

--- Lecture33 parents and siblings of an element/Lecture33.js
+++ Lecture33 parents and siblings of an element/Lecture33.js
@@ -1,1 +1,8 @@
 console.log(document.body.childNodes[1])
+console.log(document.body.firstChild)
+let a=document.body.firstChild
+console.log(a.parentNode)
+console.log(a.parentElement)
+console.log(a.firstChild)
+console.log(a.firstChild.nextSibling)
+console.log(a.lastChild)
