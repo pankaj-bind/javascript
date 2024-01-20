d7e767331ba1e2f41d70bb020ff55a2539a7d28c
Pankaj Kumar Bind <pankajbind30@gmail.com>
2024-01-20 17:02:45 +0530

    lecture 35 added

---
+++ Lecture35 table navigation/Lecture35.js
@@ -0,0 +1,7 @@
+let t = document.body.firstElementChild.firstElementChild
+console.log(t.rows)
+console.log(t.caption)
+console.log(t.tHead.firstElementChild)
+console.log(t.tFoot)
+console.log(t.tBodies)
+console.log(t.rows[0].rowIndex)
