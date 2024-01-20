6a3e6566659f69e7eb8a94456e798e4d20660cfd
Pankaj Kumar Bind <pankajbind30@gmail.com>
2024-01-20 15:47:51 +0530

    lecture 34 added

---
+++ Lecture34 element only navigation/Lecture34.js
@@ -0,0 +1,6 @@
+let b = document.body
+console.log(b.firstChild)
+console.log(b.firstElementChild)
+
+console.log(b.firstElementChild.firstElementChild)
+b.firstElementChild.firstElementChild.textContent="shjdvhshjer"
